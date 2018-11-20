dc176f03c106129c189027f2858e9c94139ac4c7
zc.shen <zhuocheng.shen@gmail.com>
2018-11-20 11:29:01 -0500

    first commit

---
+++ app/src/main/java/com/recipe/Util/Constants.java
@@ -0,0 +1,14 @@
+package com.recipe.Util;
+
+public class Constants {
+
+    //Not free anymore - I am paying $1USD/month for access
+    //For now you can use this API Key, but please note that if too many people are using this
+    // API, the app may time-out.
+    //&apikey=cdc4cef5
+
+    public static final String URL_LEFT = "http://www.omdbapi.com/?s=";
+    public static final String URL = "http://www.omdbapi.com/?i=";
+    public static final String API_KEY = "&apikey=ef57aa7a";
+    public static final String URL_RIGHT = "&page=2";
+}
